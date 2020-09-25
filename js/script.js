41e061e042efde5abc70b4e556c824000f525f9f
lapaduga <deniskotov29042015@gmail.com>
2020-09-25 19:40:41 +0500

    2nd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+	//Burger behaviour
 	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	//Slider behaviour
+	
 });
