b196e2120bec9b0b00cd405b32d8fd00df84f047
lapaduga <deniskotov29042015@gmail.com>
2020-10-02 17:05:06 +0500

    b

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	//Burger
-	$('.header__burger').click(function (event) {
+	$('.header__burger, .header__list').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
